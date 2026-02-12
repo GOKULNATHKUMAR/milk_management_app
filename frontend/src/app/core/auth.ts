0b207f7ead21b14bf056f6ce53f2386d2903e4e9
GOKULNATH K <gokulnathkumar08@gmail.com>
2026-02-12 15:38:13 +0530

    create auth service code changes commit

---
+++ frontend/src/app/core/auth.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+  private api = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  login(data: any) {
+    return this.http.post(`${this.api}/auth/login`, data);
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken() {
+    return localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+}
