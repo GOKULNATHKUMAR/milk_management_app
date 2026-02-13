0af2043558a89ace99c308cd9437b5677cc15f0e
GOKULNATH K <gokulnathkumar08@gmail.com>
2026-02-13 12:33:52 +0530

    register screen integration code changes commit

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,14 +2,20 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
+  path: '',
+  loadComponent: () =>
+    import('./home/home').then(m => m.Home)
+},
   {
     path: 'login',
     loadComponent: () =>
       import('./features/auth/login/login')
         .then(m => m.Login)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register')
+        .then(m => m.Register)
   }
 ];
